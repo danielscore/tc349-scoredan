2385cb6e33ecd1d3967ff7548283e34d941b2284
Daniel Score <scoredan@msu.edu>
2013-12-07 19:33:15 -0500

    moved js to separate file, re-did responsiveness, adding content to pages

--- final/js/main.js
+++ final/js/main.js
@@ -1,30 +1,7 @@
 $(function(){
-
-	function roll() {
-		var roll = Math.floor(Math.random() * 6) + 1;
-		if (roll == 1) {
-			$('#rolled').html(roll);
-			$('#action').html('Chaos!');
-		} else if (roll == 6) {
-			$('#rolled').html(roll);
-			$('#action').html('Chase Plane!');
-			getPlane();
-		} else {
-			$('#rolled').html(roll);
-			$('#action').html('Nothing Happens');
-		}
-	}
-
-	function getPlane() {
-		var rPlane = Math.floor(Math.random() * 74) +1;
-		var imgSrc = 'img/planes/plane'+rPlane;
-		var img = '<img src="'+imgSrc+'.jpeg" />';
-		$('.plane-pane').html(img);
-	}
-
-	getPlane();
-
-	$('#chase-plane').click(function(){
-		roll();
+	var $toCollapse = $('nav .selected').siblings();
+	$('.nav-toggle').click(function(e){
+		e.preventDefault();
+		$toCollapse.animate({height:"toggle"});
 	});
-})
+});
